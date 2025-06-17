9a6824722d1e2ef9dc37a458b0d2b409ab03d165
James Christopher Adduono <jc@adduono.com>
2025-06-17 16:52:39 -0400

    Timewalking annoyance

--- Claw.lua
+++ Claw.lua
@@ -3675,7 +3675,7 @@ end
 
 function Events:UNIT_MAXPOWER(unitId)
 	if unitId == 'player' then
-		Player.level = UnitLevel(unitId)
+		Player.level = UnitEffectiveLevel(unitId)
 		Player.mana.base = Player.BaseMana[Player.level]
 		Player.mana.max = UnitPowerMax(unitId, 0)
 		Player.rage.max = UnitPowerMax(unitId, 1)
